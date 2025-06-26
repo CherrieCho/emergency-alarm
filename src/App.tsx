a56bb86ff149ba90120c41e12f587528d8f5850b
cherrie <cherry199848@gmail.com>
2025-06-26 23:05:09 +0900

    add: app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Auth from './pages/auth/Auth'
-import LocationDetail from './pages/locationDetail/LocationDetail'
-import MainPage from './pages/mainPage/MainPage'
-<<<<<<< doyoon
-import Signup from './pages/auth/Signup'
+import { useState } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/auth/Auth';
+import LocationDetail from './pages/locationDetail/LocationDetail';
+import Signup from './pages/auth/Signup';
+import { MainPage } from './pages/mainPage/MainPage';
 
-=======
-import { MainPage } from './pages/mainPage/MainPage'
->>>>>>> dev
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<Auth />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path='/' element={<MainPage />} />
+      <Route path='/login' element={<Auth />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='/location-detail' element={<LocationDetail />} />
     </Routes>
   );
