4cb0c82e996be73d1570fe9661997a4bc019f649
cherrie <cherry199848@gmail.com>
2025-06-27 01:16:24 +0900

    add: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import Auth from './pages/auth/Auth';
 import LocationDetail from './pages/locationDetail/LocationDetail';
 import Signup from './pages/auth/Signup';
 import { MainPage } from './pages/mainPage/MainPage';
+import AppLayout from './layout/AppLayout';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
-      <Route path='/login' element={<Auth />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/location-detail' element={<LocationDetail />} />
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<MainPage />} />
+
+        <Route path='/login' element={<Auth />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/location-detail' element={<LocationDetail />} />
+      </Route>
     </Routes>
   );
 }
