e9da76a582295b0652baec0d6010d3f45e4444a4
cherrie <cherry199848@gmail.com>
2025-06-29 01:07:39 +0900

    fix: swiper css

--- src/pages/mainPage/components/MainBanner.tsx
+++ src/pages/mainPage/components/MainBanner.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Autoplay, Pagination } from 'swiper/modules';
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-// import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 import banner1 from '../../../assets/banners/heavyRain_banner.png';
-// import banner2 from '../../../assets/banners/bannerHotWeather.png';
-// import banner3 from '../../../assets/banners/wildFire_banner.png';
+import banner2 from '../../../assets/banners/bannerHotWeather.png';
+import banner3 from '../../../assets/banners/wildFire_banner.png';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import '../styles/banner.style.css';
 
 const MainBanner = () => {
@@ -18,10 +18,7 @@ const MainBanner = () => {
         height: 'auto',
       }}
     >
-      <Box>
-        <img src={banner1} />
-      </Box>
-      {/* <Swiper
+      <Swiper
         modules={[Pagination, Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
@@ -46,7 +43,7 @@ const MainBanner = () => {
             <img src={banner3} />
           </Link>
         </SwiperSlide>
-      </Swiper> */}
+      </Swiper>
     </Box>
   );
 };
