0af3dc0ed411fe4f9694d12fc4a4031564ffe990
cherrie <cherry199848@gmail.com>
2025-06-28 21:46:25 +0900

    fix: 메인 즐겨찾기 삭제

--- src/pages/mainPage/components/RegionDisasterInfo.tsx
+++ src/pages/mainPage/components/RegionDisasterInfo.tsx
@@ -1,9 +1,5 @@
 import { Box, styled, Typography, useTheme } from '@mui/material';
-import { motion } from 'framer-motion';
-import StarIcon from '@mui/icons-material/Star';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { regionMap } from './config/regionMap';
-import { useState } from 'react';
 
 const RegionBox = styled(Box)(({ theme }) => ({
   display: 'grid',
@@ -19,6 +15,7 @@ const RegionBox = styled(Box)(({ theme }) => ({
     gridTemplateColumns: 'repeat(2, 1fr)',
   },
 }));
+
 const RegionCard = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
   fontFamily: theme.typography.fontFamily,
@@ -40,46 +37,17 @@ const RegionCard = styled(Box)(({ theme }) => ({
     borderRadius: '10px',
     transition: 'transform 0.2s ease',
   },
-  '.star-icon': {
-    opacity: 0,
-    transition: 'opacity 0.2s ease',
-  },
   '&:hover': {
     cursor: 'pointer',
     backgroundColor: '#f5f5f5',
     img: {
       transform: 'translateY(-5px)',
     },
-    '.star-icon': {
-      opacity: 1,
-    },
   },
 }));
 
-/**
- * backgroundColor: theme.palette.background.default,
-  fontFamily: theme.typography.fontFamily,
- */
-
 const RegionDisasterInfo = () => {
-  // 즐겨찾기 기능 DB 로 관리? 아니면 로컬?
-  // 데이터 가져올때 즐겨찾기 true 면 앞으로 보내는 로직 만들기
-  const sortedRegions = regionMap.sort(
-    (a, b) => (b.isFavorite ? 1 : 0) - (a.isFavorite ? 1 : 0)
-  );
-  const [regions, setRegions] = useState(sortedRegions);
   const theme = useTheme();
-
-  const handleFavoriteToggle = (id: number) => {
-    const updatedRegions = regions.map((region) =>
-      region.id === id ? { ...region, isFavorite: !region.isFavorite } : region
-    );
-    updatedRegions.sort(
-      (a, b) => (b.isFavorite ? 1 : 0) - (a.isFavorite ? 1 : 0)
-    );
-
-    setRegions(updatedRegions);
-  };
 
   return (
     <Box flex={2} padding='40px'>
@@ -92,46 +60,27 @@ const RegionDisasterInfo = () => {
         지역별 정보조회
       </Typography>
       <RegionBox>
-        {regions.map((regin) => (
-          <motion.div layout transition={{ duration: 0.4 }} key={regin.id}>
-            <RegionCard>
-              <img src={regin.src} />
-              <Box
-                display='flex'
-                flexDirection='row'
-                justifyContent='space-between'
-                alignItems='center'
-                width='100%'
-                marginTop='10px'
-                height='30%'
+        {regionMap.map((region) => (
+          <RegionCard key={region.id}>
+            <img src={region.src} alt={region.name} />
+            <Box
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+              width='100%'
+              marginTop='10px'
+              height='30%'
+            >
+              <Typography
+                variant='body1'
+                sx={{
+                  fontFamily: theme.typography.fontFamily,
+                }}
               >
-                <Typography
-                  variant='body1'
-                  sx={{
-                    fontFamily: theme.typography.fontFamily,
-                    marginLeft: '5px',
-                  }}
-                >
-                  {regin.name}
-                </Typography>
-                {regin.isFavorite ? (
-                  <StarIcon
-                    className='star-icon'
-                    onClick={() => {
-                      handleFavoriteToggle(regin.id);
-                    }}
-                  />
-                ) : (
-                  <StarBorderIcon
-                    className='star-icon'
-                    onClick={() => {
-                      handleFavoriteToggle(regin.id);
-                    }}
-                  />
-                )}
-              </Box>
-            </RegionCard>
-          </motion.div>
+                {region.name}
+              </Typography>
+            </Box>
+          </RegionCard>
         ))}
       </RegionBox>
     </Box>
