2bd3ae2d9f96514b014c333c07847b1b828e2cdf
cherrie <cherry199848@gmail.com>
2025-07-03 22:52:21 +0900

    fix: routes

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -10,34 +10,40 @@ if (!JWT_SECRET) throw new Error('❌ JWT_SECRET이 설정되어 있지 않습�
 export const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: '이메일과 비밀번호를 입력해주세요.' });
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: '이메일과 비밀번호를 입력해주세요.' });
+    }
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+      return res.status(401).json({ message: '사용자를 찾을 수 없습니다.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: '비밀번호가 틀렸습니다.' });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, name: user.name, email: user.email },
+      JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({
+      message: '로그인 성공!',
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 에러' });
   }
-
-  const user = await findUserByEmail(email);
-  if (!user) {
-    return res.status(401).json({ message: '사용자를 찾을 수 없습니다.' });
-  }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(401).json({ message: '비밀번호가 틀렸습니다.' });
-  }
-
-  const token = jwt.sign(
-    { userId: user.id, name: user.name, email: user.email },
-    JWT_SECRET,
-    { expiresIn: '1h' }
-  );
-
-  res.json({
-    token,
-    user: {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    },
-  });
 });
